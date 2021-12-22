ad9e2b4da0677df475db27245ba2db34c2daecac
Textiny <96368206+Textiny@users.noreply.github.com>
2021-12-22 17:43:18 +0300

    Update bot by discord.py

--- bot by discord.py
+++ bot by discord.py
@@ -1,10 +1,13 @@
-import discord
+import discord #Импортируем библиотеку discord.py
 from discord.ext import commands
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        print('Logged on as {0}!'.format(self.user)) #Когда бот будет запущен в консоль будет написано сообщение
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        print('Message from {0.author}: {0.content}'.format(message)) #Все сообщения с серверов на котором стоит бот, будут поступать вам в консоль
+        
+client = MyClient()
+client.run('токен бота') #Запуск бота
